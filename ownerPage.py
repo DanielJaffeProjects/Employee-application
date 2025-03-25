38d377a270e8fd57cb9f89a0fac49dfcb5b0aadc
Endi <troqeendi@gmail.com>
2025-03-24 22:16:20 -0400

    fixed minor bug when launching gui

--- ownerPage.py
+++ ownerPage.py
@@ -1,7 +1,7 @@
 import tkinter as tk 
-from managerPage import ManagerPage
+from managerPage import ManageEmployees
 
-class OwnerPage(ManagerPage):
+class OwnerPage(ManageEmployees):
     def __init__(self, parent, controller):
         super().__init__(parent, controller)
         self.controller = controller
